92411cc75974e940d9af80acddba960c7c1814ea
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-31 19:48:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,8 +17,8 @@ def monopoly_with_second_tier
 	monopoly = {
 		:railroads => {
 			:pieces => 4,
-			:rent_in_dollars => {},
-			:names => {}
+			:names => {},
+			:rent_in_dollars => {}
 		}
 }
 
