439867c6d9a70d0a7ec1eb55c60b3ff763f55c0c
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-31 19:51:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,10 +29,18 @@ def monopoly_with_third_tier
 		:railroads => {
 			:pieces => 4,
 			:names => {
-				:reading_railroad => {},
-				:pennsylvania_railroad => {},
-				:b_and_o_railroad => {},
-				:shortline => {}
+				:reading_railroad => {
+					"mortgage_value" = "$100"
+				},
+				:pennsylvania_railroad => {
+					"mortgage_value" = "$200"
+				},
+				:b_and_o_railroad => {
+					"mortgage_value" = "$400"
+				},
+				:shortline => {
+					"mortgage_value" = "$800"
+				}
 			},
 			:rent_in_dollars => {
 				:one_piece_owned => 25,
